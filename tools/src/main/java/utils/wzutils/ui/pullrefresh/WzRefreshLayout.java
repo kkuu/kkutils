fe82846736b02e8b1e8fd72b95f35ee6136535b7
Gogs <gogs@fake.local>
2018-09-12 14:35:37 +0800

    。。。

--- tools/src/main/java/utils/wzutils/ui/pullrefresh/WzRefreshLayout.java
+++ tools/src/main/java/utils/wzutils/ui/pullrefresh/WzRefreshLayout.java
@@ -106,7 +106,7 @@ public class WzRefreshLayout extends SmartRefreshLayout {
         });
     }
 
-    public void bindLoadData(final LoadListDataInterface loadListDataInterface, final PageControl pageControl) {
+    public void bindLoadData(final PageControl pageControl,final LoadListDataInterface loadListDataInterface) {
         setOnRefreshLoadMoreListener(new OnRefreshLoadMoreListener() {
             @Override
             public void onLoadMore(RefreshLayout refreshLayout) {
