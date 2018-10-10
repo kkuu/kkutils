ed2eb67c040f0b19a8903cd38031c0974e2e5be2
Gogs <gogs@fake.local>
2018-10-10 14:28:52 +0800

    ..

--- tools/src/main/java/utils/wzutils/common/LanguageTool.java
+++ tools/src/main/java/utils/wzutils/common/LanguageTool.java
@@ -18,21 +18,42 @@ public class LanguageTool {
      */
     public void initLanguage(Activity activity) {
 
-        String localeStr = MapDB.loadObjByDefault("locale", String.class, "");
-        if(StringTool.notEmpty(localeStr)){
-            Locale currLocale=Locale.forLanguageTag(localeStr);
-            setLanguage(activity, null, currLocale);
+        Locale locale = MapDB.loadObjByDefault("locale", Locale.class, null);
+        if(locale!=null){
+            setLanguage(activity, null, locale);
         }
     }
 
+    /**
+     * 是否简体
+     * @return
+     */
     public boolean isSimpleChinese(){
         return Locale.SIMPLIFIED_CHINESE.equals(getLanguage());
     }
-    public boolean isEn(){
-        return Locale.ENGLISH.equals(getLanguage());
-    }
+
+    /***
+     * 是否繁体
+     * @return
+     */
     public boolean isTw(){
         return Locale.TAIWAN.equals(getLanguage());
+    }
+
+    /***
+     * 是否中文， 包含简体繁体
+     * @return
+     */
+    public boolean isChinese(){
+        return Locale.CHINESE.getLanguage().equals(getLanguage().getLanguage());
+    }
+
+    /***
+     * 是否英文
+     * @return
+     */
+    public boolean isEn(){
+        return Locale.ENGLISH.getLanguage().equals(getLanguage().getLanguage());
     }
 
     public Locale getLanguage() {
@@ -47,11 +68,12 @@ public class LanguageTool {
         Resources resources = AppTool.getApplication().getResources();// 获得res资源对象
         Configuration config = resources.getConfiguration();// 获得设置对象
         DisplayMetrics dm = resources.getDisplayMetrics();// 获得屏幕参数：主要是分辨率，像素等。
-
+        config.locale=locale;
         resources.updateConfiguration(config, dm);
         if (activity != null) activity.getResources().updateConfiguration(config, dm);
 
-        MapDB.saveObj("locale", locale.toString());
+
+        MapDB.saveObj("locale", locale);
 
         if (restartActivity != null) {
             LayoutInflaterTool.clearAll();
@@ -59,7 +81,7 @@ public class LanguageTool {
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
             AppTool.getApplication().startActivity(intent);
             android.os.Process.killProcess(android.os.Process.myPid());
-            System.exit(0);
+             System.exit(0);
         }
     }
 }
