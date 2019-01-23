7c0000bf4324b4fbaa6d596db8a57650e8858582
Gogs <gogs@fake.local>
2019-01-23 15:42:21 +0800

    ...

---
+++ tools/src/main/java/utils/wzutils/ui/tablayout/KTabLayout.java
@@ -0,0 +1,81 @@
+package utils.wzutils.ui.tablayout;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+
+import com.flyco.tablayout.SlidingTabLayout;
+
+import java.util.List;
+
+
+
+public class KTabLayout extends SlidingTabLayout {
+    public KTabLayout(Context context) {
+        super(context);
+        init();
+    }
+
+    public KTabLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public KTabLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    public void init() {
+        setIndicatorWidthEqualTitle(true);
+        setIndicatorHeight(2);
+        setTextsize(15);
+        setTabSpaceEqual(true);//这个属性好像只能在xml 里面设置
+
+        setTextSelectColor(Color.parseColor("#E2231A"));
+        setTextUnselectColor(Color.parseColor("#333333"));
+        setIndicatorColor(Color.parseColor("#E2231A"));
+    }
+
+
+    public void initViewPager(FragmentManager fragmentManager, ViewPager viewPager, final List<Fragment> fragments, List<String> titles) {
+        viewPager.setOffscreenPageLimit(fragments.size());
+        viewPager.setAdapter(new FragmentPagerAdapter(fragmentManager) {
+            @Override
+            public Fragment getItem(int i) {
+                return fragments.get(i);
+            }
+
+            @Override
+            public int getCount() {
+                return fragments.size();
+            }
+        });
+        setViewPager(viewPager, titles.toArray(new String[0]));
+    }
+    public void initViewPager(KTabLaoutBuilder kTabLaoutBuilder,FragmentManager fragmentManager, ViewPager viewPager, final List<Fragment> fragments, String ...titles) {
+        if(kTabLaoutBuilder!=null){
+            kTabLaoutBuilder.build(this);
+        }
+        viewPager.setOffscreenPageLimit(fragments.size());
+        viewPager.setAdapter(new FragmentPagerAdapter(fragmentManager) {
+            @Override
+            public Fragment getItem(int i) {
+                return fragments.get(i);
+            }
+
+            @Override
+            public int getCount() {
+                return fragments.size();
+            }
+        });
+        setViewPager(viewPager, titles);
+    }
+    public static abstract class KTabLaoutBuilder{
+        public abstract void build(KTabLayout tabLayout);
+    }
+}
