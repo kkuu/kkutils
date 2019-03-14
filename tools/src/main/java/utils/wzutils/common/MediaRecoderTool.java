328869e3d67244cf34e59813e9135191e885ab62
Gogs <gogs@fake.local>
2019-03-14 15:45:41 +0800

    视频录制变型修复

--- tools/src/main/java/utils/wzutils/common/MediaRecoderTool.java
+++ tools/src/main/java/utils/wzutils/common/MediaRecoderTool.java
@@ -7,16 +7,18 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 
+import java.util.List;
+
 /**
  * abc on 2017/6/29.
  */
 
 public class MediaRecoderTool {
-    SurfaceView surfaceView;
-    Camera camera;
-    MediaRecorder mMediaRecorder;
-    boolean isOpenCamera = false;//摄像头是否打开了
-    int rotation=0;//横屏用0  竖屏用90
+    public SurfaceView surfaceView;
+    public Camera camera;
+    public MediaRecorder mMediaRecorder;
+    public boolean isOpenCamera = false;//摄像头是否打开了
+    public int rotation=90;//横屏用0  竖屏用90
     /***
      * 视频录制才需要调用
      *
@@ -86,6 +88,10 @@ public class MediaRecoderTool {
                     camera = Camera.open();
                     //camera.getParameters().setFocusMode("auto");
                     camera.setDisplayOrientation(rotation);
+
+
+
+
                     camera.setPreviewDisplay(surfaceView.getHolder());
                     setFocus();//必须在unlock之前
                     camera.unlock();
@@ -105,11 +111,81 @@ public class MediaRecoderTool {
         // parameters.setPictureFormat(PixelFormat.JPEG);
         parameters.setFlashMode(Camera.Parameters.FLASH_MODE_AUTO);
         parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);//1连续对焦
+
+        List<Camera.Size> sizeList = parameters.getSupportedPreviewSizes();//获取所有支持的camera尺寸
+        LogTool.s("jxd","optionSize : mSurfaceView "+surfaceView.getWidth()+" * "+surfaceView.getHeight());
+        Camera.Size optionSize = getOptimalPreviewSize(sizeList, surfaceView.getHeight(), surfaceView.getWidth());//获取一个最为适配的camera.size
+        LogTool.s("jxd","optionSize : "+optionSize.width+" * "+optionSize.height);
+        parameters.setPreviewSize(optionSize.width,optionSize.height);//把camera.size赋值到parameters
+
+
+
+
         camera.setParameters(parameters);
         camera.startPreview();
         camera.cancelAutoFocus();// 2如果要实现连续的自动对焦，这一句必须加上
     }
+    /**
+     * 解决预览变形问题
+     * @param sizes
+     * @param w
+     * @param h
+     * @return
+     */
+    private Camera.Size getOptimalPreviewSize(List<Camera.Size> sizes, int w, int h) {
+        final double ASPECT_TOLERANCE = 0.1;
+        double targetRatio = (double) w / h;
+        if (sizes == null) return null;
 
+        Camera.Size optimalSize = null;
+        double minDiff = Double.MAX_VALUE;
+
+        int targetHeight = h;
+
+        // Try to find an size match aspect ratio and size
+        for (Camera.Size size : sizes) {
+            double ratio = (double) size.width / size.height;
+            if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) continue;
+            if (Math.abs(size.height - targetHeight) < minDiff) {
+                optimalSize = size;
+                minDiff = Math.abs(size.height - targetHeight);
+            }
+        }
+
+        // Cannot find the one match the aspect ratio, ignore the requirement
+        if (optimalSize == null) {
+            minDiff = Double.MAX_VALUE;
+            for (Camera.Size size : sizes) {
+                if (Math.abs(size.height - targetHeight) < minDiff) {
+                    optimalSize = size;
+                    minDiff = Math.abs(size.height - targetHeight);
+                }
+            }
+        }
+        return optimalSize;
+    }
+    /**
+     * 获取视频size
+     * @param sizes
+     * @param w
+     * @param h
+     * @return
+     */
+    private Camera.Size getOptimalVideoSize(List<Camera.Size> sizes) {
+
+        Camera.Size optimalSize = null;
+        // Try to find an size match aspect ratio and size
+        for (Camera.Size size : sizes) {
+            LogTool.s(size.width+"  "+size.height);
+        }
+        for (Camera.Size size : sizes) {
+            if(size.height>=720&&size.height<=1080){
+                LogTool.s("使用"+size.width+"  "+size.height);
+                return size;
+            }
+        }
+        return optimalSize;
+    }
     /***
      * 开始视频录制
      */
@@ -132,10 +208,12 @@ public class MediaRecoderTool {
             e.printStackTrace();
         }
     }
+    int width=1280;
+    int height=720;
     /***
      * 初始化视频录制
      */
-     void initMedia(String path) {
+    void initMedia(String path) {
         try {
             mMediaRecorder = new MediaRecorder();
             initCamera();
@@ -145,14 +223,20 @@ public class MediaRecoderTool {
             mMediaRecorder.setPreviewDisplay(surfaceView.getHolder().getSurface());
             mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);// 视频源
             mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);// 音频源
+
             mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);// 视频输出格式
             mMediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);// 音频格式
-            mMediaRecorder.setVideoSize(1280, 720);// 设置分辨率：
-            // mMediaRecorder.setVideoFrameRate(16);// 这个我把它去掉了，感觉没什么用
-            mMediaRecorder.setVideoEncodingBitRate(4 * 1280 * 720);// 设置帧频率，然后就清晰了
-            mMediaRecorder.setOrientationHint(rotation);// 输出旋转90度，保持竖屏录制,最好和摄像头保持一致
             mMediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);// 视频录制格式
+
+
+            // mMediaRecorder.setVideoFrameRate(16);// 这个我把它去掉了，感觉没什么用
+            mMediaRecorder.setOrientationHint(rotation);// 输出旋转90度，保持竖屏录制,最好和摄像头保持一致，前置摄像头要再加180，也就是270
             // mediaRecorder.setMaxDuration(Constant.MAXVEDIOTIME * 1000);
+
+            mMediaRecorder.setVideoSize(width, height);// 设置分辨率：
+            mMediaRecorder.setVideoEncodingBitRate(8 * width * height);// 设置帧频率，然后就清晰了
+
+
             mMediaRecorder.setOutputFile(path);
             mMediaRecorder.prepare();
         } catch (Exception e) {
@@ -165,7 +249,7 @@ public class MediaRecoderTool {
      * 初始化音频录制
      * @param path
      */
-     void initAudioRecoder(String path) {
+    void initAudioRecoder(String path) {
         try {
             mMediaRecorder = new MediaRecorder();
             mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);// 音频源
@@ -173,8 +257,8 @@ public class MediaRecoderTool {
             mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.AAC_ADTS);// 视频输出格式
             mMediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);// 音频格式
 
-         //   mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.AMR_WB);// 视频输出格式
-         //   mMediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_WB);// 音频格式
+            //   mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.AMR_WB);// 视频输出格式
+            //   mMediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_WB);// 音频格式
 
             mMediaRecorder.setOutputFile(path);
             mMediaRecorder.prepare();
@@ -208,7 +292,7 @@ public class MediaRecoderTool {
      */
     public static MediaPlayer mediaStartPlaySingle(String path, MediaPlayer.OnCompletionListener onCompletionListener) {
         try {
-           mediaStartPlaySingle(path,0,onCompletionListener);
+            mediaStartPlaySingle(path,0,onCompletionListener);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -254,7 +338,7 @@ public class MediaRecoderTool {
     }
     public static boolean mediaIsPlayingSingle() {
         try {
-           return mediaPlayer.isPlaying();
+            return mediaPlayer.isPlaying();
         } catch (Exception e) {
             e.printStackTrace();
         }
