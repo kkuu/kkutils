763e9c249fc1f3192595cf8cc9d8b4ddbfcb1b20
Gogs <gogs@fake.local>
2019-06-24 14:59:32 +0800

    版本更新问题 参考https://blog.csdn.net/Android_Study_OK/article/details/82414659

--- tools/src/main/java/utils/kkutils/update/Version.java
+++ tools/src/main/java/utils/kkutils/update/Version.java
@@ -7,11 +7,15 @@ import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
+
 import androidx.appcompat.app.AlertDialog;
+import androidx.core.content.FileProvider;
 
 import java.io.File;
 
 import utils.kkutils.AppTool;
+import utils.kkutils.BuildConfig;
 import utils.kkutils.R;
 import utils.kkutils.common.FileTool;
 import utils.kkutils.common.LogTool;
@@ -37,7 +41,7 @@ public class Version {
      * @param newVersion
      */
     public static boolean checkUpDate(final Context context, final Version newVersion) {
-        int currVersionCode = getCurrVersionCode(context);
+        int currVersionCode = newVersion.getCurrVersionCode(context);
         LogTool.s("检查更新： 当前版本" + currVersionCode + "  服务端版本" + newVersion.versionCode);
         boolean hasNew = currVersionCode < newVersion.versionCode;
         newVersion.updateDesc = "" + newVersion.updateDesc.replace("\\n", "\n");
@@ -78,7 +82,7 @@ public class Version {
             dialogShowMsg.setButton(ProgressDialog.BUTTON_POSITIVE, context.getString(R.string.liji_gengxin), new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
-                    showDownLoadProgressDialog(context, newVersion, isForce);
+                   newVersion. showDownLoadProgressDialog(context, newVersion, isForce);
                 }
             });
 
@@ -93,7 +97,7 @@ public class Version {
      * @param context
      * @param newVersion
      */
-    public static void showDownLoadProgressDialog(final Context context, Version newVersion, final boolean isForce) {
+    public  void showDownLoadProgressDialog(final Context context, Version newVersion, final boolean isForce) {
         final ProgressDialog progressDialog = new ProgressDialog(context);
         progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
         progressDialog.setMax(100);
@@ -144,7 +148,7 @@ public class Version {
      * @param context
      * @return
      */
-    public static int getCurrVersionCode(Context context) {
+    public  int getCurrVersionCode(Context context) {
         try {
             PackageManager pm = context.getPackageManager();
             PackageInfo info = pm.getPackageInfo(context.getPackageName(), 0);
@@ -161,15 +165,33 @@ public class Version {
      * @param context
      * @param path
      */
-    private static void installApk(final Context context, final String path) {
+    private  void installApk(final Context context, final String path) {
 
 
-        LogTool.s("installApk ing " + path + "  " + new File(path).length());
-        Intent intent = new Intent(Intent.ACTION_VIEW);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.setDataAndType(Uri.parse("file://" + path), "application/vnd.android.package-archive");
+        if(true)autoInstallApk(context,new File(path));
+//        LogTool.s("installApk ing " + path + "  " + new File(path).length());
+//        Intent intent = new Intent(Intent.ACTION_VIEW);
+//        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//        intent.setDataAndType(Uri.parse("file://" + path), "application/vnd.android.package-archive");
+//        context.startActivity(intent);
+
+
+    }
+
+    private  void autoInstallApk(Context context,File file) {
+        LogTool.s("installApk ing " + file.getAbsolutePath() + "  " + file.length());
+
+        Intent intent = new Intent(Intent.ACTION_INSTALL_PACKAGE);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            intent.setDataAndType(Uri.fromFile(file), "application/vnd.android.package-archive");
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        } else {
+            // 声明需要的临时的权限
+            intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            // 第二个参数，即第一步中配置的authorities
+            Uri contentUri = FileProvider.getUriForFile(AppTool.getApplication(), BuildConfig.APPLICATION_ID + ".fileprovider", file);//注意这里，我故意写了com.http.www.smarthttpdemo.BuildConfig，因为这里出错了我郁闷了好久，因为这里容易导错包，如果你是多module开发，一定要使用主Module（即：app）的那个BuildConfig（这里说明一下，我上面的清单文件是主Module的，所以这里也要导包导入主module的，我觉得写到被依赖module也是可以的，但要保持一致，这个我没试过）
+            intent.setDataAndType(contentUri, "application/vnd.android.package-archive");
+        }
         context.startActivity(intent);
-
-
     }
 }
