566b2a90b1d7d1baf465a5555c1348b6224a46bf
Gogs <gogs@fake.local>
2019-08-19 14:12:33 +0800

    圆形图片

---
+++ tools/src/main/java/utils/kkutils/ui/clicp/CircleImageView.java
@@ -0,0 +1,29 @@
+package utils.kkutils.ui.clicp;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+
+import utils.kkutils.ui.KKImageView;
+
+public class CircleImageView extends RoundImageView {
+
+
+    public CircleImageView(Context context) {
+        super(context);
+    }
+
+    public CircleImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CircleImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public void init() {
+        super.init();
+        roundViewTool.mRoundAsCircle=true;
+    }
+}
