795ef86e65f9c7d67def4b31d6293501c8164012
Gogs <gogs@fake.local>
2019-10-24 10:06:39 +0800

    底部弹框

--- tools/src/main/java/utils/kkutils/ui/dialog/DialogSimple.java
+++ tools/src/main/java/utils/kkutils/ui/dialog/DialogSimple.java
@@ -12,6 +12,8 @@ import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.widget.TextView;
 
+import java.util.Arrays;
+
 import androidx.annotation.RequiresApi;
 import utils.kkutils.AppTool;
 import utils.kkutils.R;
@@ -114,7 +116,7 @@ public class DialogSimple {
      * @param items
      * @return
      */
-    public static Dialog showBottomChooseDialog(String title, final OnBottomChooseDialogClick onclickListener, String ... items){
+    public static Dialog showBottomChooseDialog(String title, final OnBottomChooseDialogClick onclickListener, final String ... items){
         View view= LayoutInflaterTool.getInflater(3,R.layout.kk_dialog_dibu_xuanze).inflate();
         final Dialog dialog = DialogTool.initBottomDialog(view);
 
@@ -122,22 +124,27 @@ public class DialogSimple {
 
         ViewGroup kk_vg_dialog_dibu_xuanze_items=dialog.findViewById(R.id.kk_vg_dialog_dibu_xuanze_items);
         kk_vg_dialog_dibu_xuanze_items.removeAllViews();
-        for(final String item:items){
+
+
+        for(int i=0;i<items.length;i++){
+            final String item=items[i];
             TextView textView=new TextView(new ContextThemeWrapper(AppTool.getApplication(),R.style.kk_tv_dibu_dialog_xuanze_btn));
             UiTool.setTextView(textView,item);
             View lineView=new View(new ContextThemeWrapper(AppTool.getApplication(),R.style.kk_line_h));
             lineView.setBackgroundColor(AppTool.getApplication().getResources().getColor(R.color.kk_line));
             kk_vg_dialog_dibu_xuanze_items.addView(lineView,new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,1));
             kk_vg_dialog_dibu_xuanze_items.addView(textView,new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, CommonTool.dip2px(58)));
+            final int index = i;
             textView.setOnClickListener(new KKViewOnclickListener() {
                 @Override
                 public void onClickKK(View v) {
                     if(onclickListener!=null){
-                        onclickListener.onClick(dialog,item);
+                        onclickListener.onClick(dialog,items, index,item);
                     }
                 }
             });
         }
+
         view.findViewById(R.id.kk_tv_dialog_dibu_xuanze_quxiao).setOnClickListener(new KKViewOnclickListener() {
             @Override
             public void onClickKK(View v) {
@@ -148,6 +155,6 @@ public class DialogSimple {
         return dialog;
     }
     public static interface OnBottomChooseDialogClick{
-        public void onClick(Dialog dialog,String item);
+        public void onClick(Dialog dialog,String[] items,int index,String item);
     }
 }
