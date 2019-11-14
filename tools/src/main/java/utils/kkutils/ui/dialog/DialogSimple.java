bb1b23e2ae4faeaf267a6d33be30a369523560dd
Gogs <gogs@fake.local>
2019-11-14 10:16:28 +0800

    底部弹框 可选择title

--- tools/src/main/java/utils/kkutils/ui/dialog/DialogSimple.java
+++ tools/src/main/java/utils/kkutils/ui/dialog/DialogSimple.java
@@ -37,20 +37,20 @@ public class DialogSimple {
      * @param onclickListenerQuXiao
      * @return
      */
-    public static Dialog showTiShiDialog(String title, String content, String btnQueDingText, final KKViewOnclickListener onclickListenerQueDing,String btnQuXiaoText,final KKViewOnclickListener onclickListenerQuXiao){
-        Dialog dialog=showTiShiDialogImp(R.layout.kk_dialog_tishi,title,btnQueDingText,onclickListenerQueDing,btnQuXiaoText,onclickListenerQuXiao);
-        final TextView tv_dialog_content=dialog.findViewById(R.id.kk_tv_dialog_content);
-        UiTool.setTextView(tv_dialog_content,content);
+    public static Dialog showTiShiDialog(String title, String content, String btnQueDingText, final KKViewOnclickListener onclickListenerQueDing, String btnQuXiaoText, final KKViewOnclickListener onclickListenerQuXiao) {
+        Dialog dialog = showTiShiDialogImp(R.layout.kk_dialog_tishi, title, btnQueDingText, onclickListenerQueDing, btnQuXiaoText, onclickListenerQuXiao);
+        final TextView tv_dialog_content = dialog.findViewById(R.id.kk_tv_dialog_content);
+        UiTool.setTextView(tv_dialog_content, content);
 
         tv_dialog_content.getViewTreeObserver().addOnDrawListener(new ViewTreeObserver.OnDrawListener() {
             @Override
             public void onDraw() {
                 try {
                     int lineCount = tv_dialog_content.getLineCount();
-                    if(lineCount<2){
+                    if (lineCount < 2) {
                         tv_dialog_content.setGravity(Gravity.CENTER);
                     }
-                }catch (Exception e){
+                } catch (Exception e) {
                     LogTool.ex(e);
                 }
 
@@ -60,22 +60,22 @@ public class DialogSimple {
     }
 
 
-    public static Dialog showTiShiDialogImp(int layoutId,String title, String btnQueDingText, final KKViewOnclickListener onclickListenerQueDing,String btnQuXiaoText,final KKViewOnclickListener onclickListenerQuXiao){
-        View view= AppTool.currActivity.getLayoutInflater().inflate(layoutId,null);
+    public static Dialog showTiShiDialogImp(int layoutId, String title, String btnQueDingText, final KKViewOnclickListener onclickListenerQueDing, String btnQuXiaoText, final KKViewOnclickListener onclickListenerQuXiao) {
+        View view = AppTool.currActivity.getLayoutInflater().inflate(layoutId, null);
         final Dialog dialog = DialogTool.initNormalDialog(view, -1);
-        UiTool.setTextView(view,R.id.kk_tv_dialog_title,title);
+        UiTool.setTextView(view, R.id.kk_tv_dialog_title, title);
 
 
-        final TextView btn_dialog_queding=view.findViewById(R.id.kk_btn_dialog_queding);
-        UiTool.setTextView(btn_dialog_queding,btnQueDingText);
+        final TextView btn_dialog_queding = view.findViewById(R.id.kk_btn_dialog_queding);
+        UiTool.setTextView(btn_dialog_queding, btnQueDingText);
         btn_dialog_queding.setOnClickListener(new KKViewOnclickListener() {
             @Override
             public void onClickKK(View v) {
                 try {
-                    if(onclickListenerQueDing!=null){
+                    if (onclickListenerQueDing != null) {
                         onclickListenerQueDing.onClickKK(v);
                     }
-                }catch (Exception e){
+                } catch (Exception e) {
                     LogTool.ex(e);
                 }
                 dialog.dismiss();
@@ -83,20 +83,20 @@ public class DialogSimple {
         });
 
 
-        final TextView btn_dialog_quxiao=view.findViewById(R.id.kk_btn_dialog_quxiao);
-        if(btn_dialog_quxiao!=null){
-            if(StringTool.isEmpty(btnQuXiaoText)){
+        final TextView btn_dialog_quxiao = view.findViewById(R.id.kk_btn_dialog_quxiao);
+        if (btn_dialog_quxiao != null) {
+            if (StringTool.isEmpty(btnQuXiaoText)) {
                 btn_dialog_quxiao.setVisibility(View.GONE);
-            }else {
-                UiTool.setTextView(btn_dialog_quxiao,btnQuXiaoText);
+            } else {
+                UiTool.setTextView(btn_dialog_quxiao, btnQuXiaoText);
                 btn_dialog_quxiao.setOnClickListener(new KKViewOnclickListener() {
                     @Override
                     public void onClickKK(View v) {
                         try {
-                            if(onclickListenerQuXiao!=null){
+                            if (onclickListenerQuXiao != null) {
                                 onclickListenerQuXiao.onClickKK(v);
                             }
-                        }catch (Exception e){
+                        } catch (Exception e) {
                             LogTool.ex(e);
                         }
                         dialog.dismiss();
@@ -116,36 +116,47 @@ public class DialogSimple {
      * @param items
      * @return
      */
-    public static Dialog showBottomChooseDialog(String title, final OnBottomChooseDialogClick onclickListener, final String ... items){
+    public static Dialog showBottomChooseDialog(String title, final OnBottomChooseDialogClick onclickListener, final String... items) {
 
 
-        View view= LayoutInflaterTool.getInflater(3,R.layout.kk_dialog_dibu_xuanze).inflate();
+        View view = LayoutInflaterTool.getInflater(3, R.layout.kk_dialog_dibu_xuanze).inflate();
         final Dialog dialog = DialogTool.initBottomDialog(view);
-        if(StringTool.isEmpty(title)){
-            UiTool.getParentView(view.findViewById(R.id.kk_tv_dialog_dibu_xuanze_title)).setVisibility(View.GONE);
-        }
-        UiTool.setTextView(view,R.id.kk_tv_dialog_dibu_xuanze_title,title);
-        ViewGroup kk_vg_dialog_dibu_xuanze_items=dialog.findViewById(R.id.kk_vg_dialog_dibu_xuanze_items);
-        kk_vg_dialog_dibu_xuanze_items.removeAllViews();
-        for(int i=0;i<items.length;i++){
-            final String item=items[i];
-            TextView textView=new TextView(new ContextThemeWrapper(AppTool.getApplication(),R.style.kk_tv_dibu_dialog_xuanze_btn));
-            UiTool.setTextView(textView,item);
-            View lineView=new View(new ContextThemeWrapper(AppTool.getApplication(),R.style.kk_line_h));
-            lineView.setBackgroundColor(AppTool.getApplication().getResources().getColor(R.color.kk_line));
-            kk_vg_dialog_dibu_xuanze_items.addView(textView,new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, CommonTool.dip2px(58)));
-            if(i!=items.length-1){
-                kk_vg_dialog_dibu_xuanze_items.addView(lineView,new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,1));
+        {//标题
+            if (StringTool.isEmpty(title)) {
+                UiTool.getParentView(view.findViewById(R.id.kk_tv_dialog_dibu_xuanze_title)).setVisibility(View.GONE);
             }
-            final int index = i;
-            textView.setOnClickListener(new KKViewOnclickListener() {
-                @Override
-                public void onClickKK(View v) {
-                    if(onclickListener!=null){
-                        onclickListener.onClick(dialog,items, index,item);
+            UiTool.setTextView(view, R.id.kk_tv_dialog_dibu_xuanze_title, title);
+        }
+
+
+        ViewGroup kk_vg_dialog_dibu_xuanze_items = dialog.findViewById(R.id.kk_vg_dialog_dibu_xuanze_items);
+        kk_vg_dialog_dibu_xuanze_items.removeAllViews();
+
+        for (int i = 0; i < items.length; i++) {
+            final String item = items[i];
+            {//加文本
+                TextView textView = new TextView(new ContextThemeWrapper(AppTool.getApplication(), R.style.kk_tv_dibu_dialog_xuanze_btn));
+                UiTool.setTextView(textView, item);
+                kk_vg_dialog_dibu_xuanze_items.addView(textView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, CommonTool.dip2px(58)));
+                final int index = i;
+                textView.setOnClickListener(new KKViewOnclickListener() {
+                    @Override
+                    public void onClickKK(View v) {
+                        if (onclickListener != null) {
+                            onclickListener.onClick(dialog, items, index, item);
+                        }
                     }
+                });
+            }
+            {//加线
+                if (i != items.length - 1) {
+                    View lineView = new View(new ContextThemeWrapper(AppTool.getApplication(), R.style.kk_line_h));
+                    lineView.setBackgroundColor(AppTool.getApplication().getResources().getColor(R.color.kk_line));
+                    kk_vg_dialog_dibu_xuanze_items.addView(lineView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 1));
                 }
-            });
+            }
+
+
         }
 
         view.findViewById(R.id.kk_tv_dialog_dibu_xuanze_quxiao).setOnClickListener(new KKViewOnclickListener() {
@@ -157,7 +168,8 @@ public class DialogSimple {
         dialog.show();
         return dialog;
     }
-    public static interface OnBottomChooseDialogClick{
-        public void onClick(Dialog dialog,String[] items,int index,String item);
+
+    public static interface OnBottomChooseDialogClick {
+        public void onClick(Dialog dialog, String[] items, int index, String item);
     }
 }
