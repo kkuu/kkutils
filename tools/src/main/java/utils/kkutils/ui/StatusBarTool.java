ea0ed1bd4d882e4068ec9bc2652f62ec303dac22
Gogs <gogs@fake.local>
2019-12-30 17:41:50 +0800

    透明状态栏

--- tools/src/main/java/utils/kkutils/ui/StatusBarTool.java
+++ tools/src/main/java/utils/kkutils/ui/StatusBarTool.java
@@ -32,20 +32,31 @@ public class StatusBarTool {
      */
     public static void setStatusBarColor(Activity activity,int bgColor, boolean isTvBlack,boolean isLayoutBelowToStatusBar){
         try {
+            setStatusBarColor(activity.getWindow(),bgColor,isTvBlack,isLayoutBelowToStatusBar);
+        }catch (Exception e){
+            LogTool.ex(e);
+        }
+    }
+
+    /***
+     * 设置状态栏颜色,和文字颜色
+     * @param  isLayoutBelowToStatusBar   contentView 是否在 状态栏的下面一层
+     */
+    public static void setStatusBarColor(Window window, int bgColor, boolean isTvBlack, boolean isLayoutBelowToStatusBar){
+        try {
             bgColor= ResourcesTool.getColor(bgColor);
-            LogTool.s("setStatusBarColor "+activity);
             int option = View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
             if(!isLayoutBelowToStatusBar)option=0;
             {
-                activity.getWindow().setStatusBarColor(bgColor);
+                window.setStatusBarColor(bgColor);
                 // 设置状态栏底色白色
-                activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-                activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
 
                 if(isTvBlack){  // 设置状态栏字体黑色
-                    activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR|option);
+                    window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR|option);
                 }else {
-                    activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE|option);
+                    window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE|option);
                 }
             }
         }catch (Exception e){
@@ -54,106 +65,5 @@ public class StatusBarTool {
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    /***
-     * 设置状态栏颜色,不要设置透明， 透明用
-     * @see  StatusBarTool#setStatusBarTouMing(Activity)
-     * @param activity
-     * @param color
-     * @param  isLayoutFullScreen   contentView 是否在 状态栏的下面一层
-     */
-    public static void setStatusBarColor(Activity activity, int color,boolean isLayoutFullScreen){
-        try {
-            LogTool.s("setStatusBarColor "+activity);
-            boolean isLight= ColorUtils.calculateLuminance(color)>=0.5;
-
-            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M){//android 6 上才可设置状态栏浅色， 对应文字深色， android 5 没法改文字颜色，所以不能用浅色
-                if(isLight){
-                    color=getDarkColorPrimary(activity);//使用系统深色背景
-                }
-            }
-
-            {
-                activity.getWindow().setStatusBarColor(color);
-                // 设置状态栏底色白色
-                activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-                activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-                if(isLight){
-                    // 设置状态栏字体黑色
-                    if(isLayoutFullScreen){
-                        activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR|getSystemUiOptionFullscreen());
-                    }else {
-                        activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-                    }
-                }else {
-                    if(isLayoutFullScreen){
-                        activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE|getSystemUiOptionFullscreen());
-                    }else {
-                        activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
-                    }
-                }
-            }
-        }catch (Exception e){
-            LogTool.ex(e);
-        }
-    }
-
-    public static void setStatusBarColor(Activity activity, int color){
-        setStatusBarColor(activity,color,false);
-    }
-
-
-
-    /***
-     * 设置状态栏透明 自带SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-     * @param activity
-     */
-    public static void setStatusBarTouMing(Activity activity){
-        LogTool.s("setStatusBarTouMing "+activity);
-        Window window = activity.getWindow();
-
-        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);//设置状态栏颜色白色
-        window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        window.setBackgroundDrawable(null);
-        BarUtils.setStatusBarColor(activity, Color.TRANSPARENT);
-    }
-    /***
-     * 设置状态栏半透明 自带SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-     * @param activity
-     */
-    public static void setStatusBarBanTouMing(Activity activity){
-        setStatusBarTouMing(activity);
-        BarUtils.setStatusBarColor(activity, Color.parseColor("#55000000"));
-
-    }
-
-
-
-
-
-
-
-    static int getSystemUiOptionFullscreen(){
-        int option = View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
-        return option;
-    }
 
 }
