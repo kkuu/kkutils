d1ba8a248ac13249340156905a7716373c09ac82
Gogs <gogs@fake.local>
2020-05-06 15:22:51 +0800

    可设置点击间隔

--- tools/src/main/java/utils/kkutils/parent/KKViewOnclickListener.java
+++ tools/src/main/java/utils/kkutils/parent/KKViewOnclickListener.java
@@ -11,13 +11,13 @@ import utils.kkutils.common.LogTool;
 public abstract class KKViewOnclickListener implements View.OnClickListener {
     static  long timePreClick=0;
     boolean allowQuickClick;
-
+    long quickClickTime=300;//间隔300毫秒才能点击
     @Override
     public void onClick(View v) {
         try {
             if(!allowQuickClick){
                 long time=System.currentTimeMillis();
-                if(time-timePreClick<300)return;//300毫秒内只能点击一次
+                if(time-timePreClick<quickClickTime)return;//300毫秒内只能点击一次
                 timePreClick=time;
             }
             onClickKK(v);
@@ -27,6 +27,10 @@ public abstract class KKViewOnclickListener implements View.OnClickListener {
     }
     public KKViewOnclickListener(boolean allowQuickClick){
         setAllowQuickClick(allowQuickClick);
+    }
+    public KKViewOnclickListener(long  quickClickTime){
+        setAllowQuickClick(false);
+        this.quickClickTime=quickClickTime;
     }
     public KKViewOnclickListener(){
     }
