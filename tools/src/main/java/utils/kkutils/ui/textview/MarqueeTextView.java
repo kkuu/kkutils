502a6eb83bb69f41974a6df93c65df2bf3a88c79
Gogs <gogs@fake.local>
2020-09-08 10:49:28 +0800

    自动滚动textview

--- tools/src/main/java/utils/kkutils/ui/textview/MarqueeTextView.java
+++ tools/src/main/java/utils/kkutils/ui/textview/MarqueeTextView.java
@@ -1,16 +1,12 @@
 package utils.kkutils.ui.textview;
 
-
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Rect;
 import android.text.TextUtils;
 import android.util.AttributeSet;
 
-/**
- * abc on 2017/11/16.
- */
-
 public class MarqueeTextView extends androidx.appcompat.widget.AppCompatTextView {
-
     public MarqueeTextView(Context context) {
         super(context);
         init();
@@ -23,16 +19,27 @@ public class MarqueeTextView extends androidx.appcompat.widget.AppCompatTextView
 
     public MarqueeTextView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init();
-    }
 
+        init();
+
+
+    }
     public void init(){
         setSingleLine();
         setEllipsize(TextUtils.TruncateAt.MARQUEE);
+        setFocusable(true);
+        setMarqueeRepeatLimit(-1);
     }
+
 
     @Override
     public boolean isFocused() {
         return true;
+    }
+
+    @SuppressLint("MissingSuperCall")
+    @Override
+    protected void onFocusChanged(boolean focused, int direction, Rect previouslyFocusedRect) {
+        //super.onFocusChanged(focused, direction, previouslyFocusedRect);
     }
 }
