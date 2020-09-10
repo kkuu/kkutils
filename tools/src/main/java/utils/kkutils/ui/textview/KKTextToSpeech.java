9f886326854a915c425b76c64f8b9a0fd8b8f6b9
Gogs <gogs@fake.local>
2020-09-10 10:32:08 +0800

    语音播报

---
+++ tools/src/main/java/utils/kkutils/ui/textview/KKTextToSpeech.java
@@ -0,0 +1,35 @@
+package utils.kkutils.ui.textview;
+
+import android.content.Context;
+import android.speech.tts.TextToSpeech;
+
+import utils.kkutils.AppTool;
+import utils.kkutils.common.LogTool;
+
+public class KKTextToSpeech {
+
+
+    TextToSpeech mTTS;
+    public KKTextToSpeech(String text){
+         mTTS = new TextToSpeech(AppTool.getApplication(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                speed(text);
+            }
+        });
+
+    }
+    public void speed(String text){
+        try {
+            mTTS.setSpeechRate(1);
+            mTTS.setPitch(1);
+            mTTS.speak(text, TextToSpeech.QUEUE_FLUSH,null);
+        }catch (Exception e){
+            LogTool.ex(e);
+        }
+
+    }
+
+
+
+}
